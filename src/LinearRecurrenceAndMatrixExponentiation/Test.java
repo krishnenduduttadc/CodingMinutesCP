80f9478d984222fbb47eabb64b97a9f2888fd714
krishnendu <krishnenduduttadc@gmail.com>
2023-06-01 20:51:09 +0530

    first commit

---
+++ src/LinearRecurrenceAndMatrixExponentiation/Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinearRecurrenceAndMatrixExponentiation;
+
+/**
+ *
+ * @author krish
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a=7;
+        int b=7>>1;
+        //System.out.println(""+b);
+                
+        int l=100;
+        while(l>0){
+            l=l>>1;
+            System.out.println(""+l);
+        }
+    }
+            
+}
